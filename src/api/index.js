16d9036f42d448baa531c64589d70ef05275d3fd
koberidzemikheili <mikheil.koberidze@redberry.ge>
2023-06-28 12:43:42 +0400

    feat: add isseen functionality to notifications

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+import router from "@/router";
+import { useUserStore } from "@/stores/user.js";
+
 const backendurl = import.meta.env.VITE_PUBLIC_BACKEND_URL;
 const locale = localStorage.getItem("last-locale") || "en";
+
 const instance = axios.create({
   baseURL: backendurl,
   withCredentials: true,
@@ -12,4 +16,19 @@ const instance = axios.create({
     "Accept-Language": locale,
   },
 });
+
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      const userStore = useUserStore(); // Define userStore here
+      userStore.logout();
+      router.push({ name: "LoginPage" });
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default instance;
--- src/components/TheNotifications.vue
+++ src/components/TheNotifications.vue
@@ -1,45 +1,64 @@
 <template>
   <div class="relative">
     <button
-      class="py-1 px-4 mx-2 text-white"
+      class="relative py-1 px-4 mx-2 text-white"
       @click="showNotifications = !showNotifications"
     >
       <IconBell class="w-6" />
+      <div
+        v-if="unseenNotifications"
+        class="absolute top-1 right-2 w-5 h-5 rounded-full bg-orange-600 text-center text-white flex items-center justify-center"
+      >
+        {{ unseenNotifications }}
+      </div>
     </button>
+
     <div
       class="absolute right-0 w-96 mt-2 py-2 px-6 bg-black rounded-lg shadow-xl text-white"
       v-show="showNotifications"
     >
       <div class="py-2 flex justify-between items-center">
         <span class="text-lg font-bold">Notifications</span>
-        <button class="underline">Mark all as read</button>
+        <button class="underline" @click="markAllAsRead">
+          Mark all as read
+        </button>
       </div>
-      <div
-        class="border border-gray-500 px-4 py-2 my-2 rounded"
-        v-for="(notification, index) in notifications"
-        :key="index"
-      >
-        <div class="flex items-center justify-between">
-          <div class="flex items-center">
-            <img
-              class="w-12 h-12 rounded-full bg-gray-300 mr-2"
-              :src="
-                backendurl + '/storage/' + notification.actor.profile_pictures
-              "
-            />
-            <div>
-              <div>{{ notification.actor.username }}</div>
-              <div class="text-sm text-gray-400">
-                <div v-if="notification.action === 'like'">
-                  <IconHeartFill />
-                </div>
-                <div v-if="notification.action === 'comment'">
-                  <IconChatQuote />
+      <div class="max-h-96 overflow-auto no-scrollbar">
+        <div
+          class="border border-gray-500 px-4 py-2 my-2 rounded"
+          v-for="(notification, index) in notifications"
+          :key="index"
+        >
+          <div
+            @click="isQuoteSeen(notification.id, notification.quote_id)"
+            class="flex items-center justify-between"
+          >
+            <div class="flex items-center">
+              <img
+                class="w-12 h-12 rounded-full bg-gray-300 mr-2"
+                :src="
+                  backendurl + '/storage/' + notification.actor.profile_pictures
+                "
+              />
+              <div>
+                <div>{{ notification.actor.username }}</div>
+                <div class="text-sm text-gray-400">
+                  <div v-if="notification.action === 'like'">
+                    <IconHeartFill />
+                  </div>
+                  <div v-if="notification.action === 'comment'">
+                    <IconChatQuote />
+                  </div>
                 </div>
               </div>
             </div>
+            <div class="text-center">
+              <div>{{ getTimeDifference(notification.created_at) }}</div>
+              <div class="text-green-600" v-if="notification.is_seen === 0">
+                New
+              </div>
+            </div>
           </div>
-          <div>{{ getTimeDifference(notification.created_at) }}</div>
         </div>
       </div>
     </div>
@@ -50,14 +69,49 @@
 import IconBell from "@/components/icons/IconBell.vue";
 import IconChatQuote from "@/components/icons/IconChatQuote.vue";
 import IconHeartFill from "@/components/icons/IconHeartFill.vue";
-import { ref } from "vue";
+import instance from "@/api/index.js";
+import router from "@/router";
+import { ref, computed } from "vue";
+import { useUserStore } from "@/stores/user.js";
+
+const userStore = useUserStore();
 const backendurl = import.meta.env.VITE_PUBLIC_BACKEND_URL;
 
 const showNotifications = ref(false);
 
-defineProps({
+const props = defineProps({
   notifications: { type: Array, required: true },
 });
+
+const isQuoteSeen = (notificationId, quoteId) => {
+  instance
+    .post(`/api/notifications/${notificationId}`)
+    .then(() => {
+      router.push({ name: "ViewQuote", params: { id: quoteId } });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+const markAllAsRead = () => {
+  const formData = { userId: userStore.userData.id };
+  instance
+    .post(`/api/notifications-mark-all-as-seen`, formData)
+    .then(() => {
+      userStore.fetchUserData();
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+const unseenNotifications = computed(() => {
+  return props.notifications.filter(
+    (notification) => notification.is_seen === 0
+  ).length;
+});
+
 function getTimeDifference(date) {
   const currentDate = new Date();
   const notificationDate = new Date(date);
